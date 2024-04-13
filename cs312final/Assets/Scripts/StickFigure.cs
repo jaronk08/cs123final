5294df00b3765820f7184f976dc8d64000df220d
jaronk08 <jaronkellyleague@hotmail.com>
2024-04-13 13:51:53 -0400

    Added Shooting, Layers, Tags.

--- cs312final/Assets/Scripts/StickFigure.cs
+++ cs312final/Assets/Scripts/StickFigure.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class StickFigure : MonoBehaviour
@@ -8,9 +9,14 @@ public class StickFigure : MonoBehaviour
 
     [Header("Set in Inspector")]
     public float speed = 30f;
+    public float projectileSpeed = 30f;
+    public float fireDelay = 1;
 
     [Header("Set Dynamically")]
     Rigidbody rigidBody;
+    public GameObject projectilePrefab;
+    private float lastFire;
+    
 
     void Start()
     {
@@ -19,10 +25,51 @@ public class StickFigure : MonoBehaviour
 
     void Update()
     {
+        //player
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
         rigidBody.velocity = new Vector3(horizontal * speed, 0f, vertical * speed);
+
+        //projectile shooting
+        if(Time.time > lastFire + fireDelay)
+        {
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                Shoot(Vector3.forward);
+                lastFire = Time.time;
+            }
+            else if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                Shoot(Vector3.back);
+                lastFire = Time.time;
+            }
+            else if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                Shoot(Vector3.left);
+                lastFire = Time.time;
+            }
+            else if (Input.GetKey(KeyCode.RightArrow))
+            {
+                Shoot(Vector3.right);
+                lastFire = Time.time;
+            }
+        }
+    }
+
+    void Shoot(Vector3 direction)
+    {
+        Vector3 shootingPosition = transform.position;
+        shootingPosition.y = shootingPosition.y + 3;
+
+        //create projectile
+        GameObject projectile = Instantiate(
+            projectilePrefab, shootingPosition, transform.rotation) as GameObject;
+
+        Rigidbody rigidBody =  projectile.GetComponent<Rigidbody>();
+
+        rigidBody.velocity = direction * projectileSpeed;
+
     }
 
 }
